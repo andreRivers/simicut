4deab9fd5f37d8aff615d72ae0efb0d08f8a18f1
PowerRanger <triandre04@gmail.com>
2021-01-28 13:55:24 +0700

    Update Ke master Host

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,8 @@
 
 	 <!-- Bootstrap 3.3.7 -->
 	 <link rel="shortcut icon" type="image/icon" href="<?= base_url('assets/'); ?>img/icon.png">
+	 
+	 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	 <link rel="stylesheet" href="<?= base_url('assets/'); ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?= base_url('assets/'); ?>bower_components/font-awesome/css/font-awesome.min.css">
@@ -30,7 +32,7 @@
 		<link rel="stylesheet" href="<?= base_url('assets/'); ?>bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="<?= base_url('assets/'); ?>dist/css/skins/skin-purple.css">
+	<link rel="stylesheet" href="<?= base_url('assets/'); ?>dist/css/skins/_all-skins.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
